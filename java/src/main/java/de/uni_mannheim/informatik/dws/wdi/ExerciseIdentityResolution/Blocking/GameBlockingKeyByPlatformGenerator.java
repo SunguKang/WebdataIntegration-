16d388cd3ddad9beb9dfa6f5f04984c6a3958f9e
leowerne <leowerne@mail.uni-mannheim.de>
2022-11-13 22:01:32 +0100

    inclueded java progeress from 13.11.

---
+++ java/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Blocking/GameBlockingKeyByPlatformGenerator.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Data and Web Science Group, University of Mannheim, Germany (http://dws.informatik.uni-mannheim.de/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Game;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.BlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.RecordBlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.Pair;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class GameBlockingKeyByPlatformGenerator extends
+			RecordBlockingKeyGenerator<Game, Attribute> {
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	public void generateBlockingKeys(Game record, Processable<Correspondence<Attribute, Matchable>> correspondences,
+			DataIterator<Pair<String, Game>> resultCollector) {
+		
+		//resultCollector.next(new Pair<>(record.getPlatform(), record));
+		//include year here because lazy, only include year if it is not null
+		if (null != record.getPublicationDate()) { 
+		resultCollector.next(new Pair<>(record.getPlatform()+Integer.toString(record.getPublicationDate().getYear()-1), record));
+		resultCollector.next(new Pair<>(record.getPlatform()+Integer.toString(record.getPublicationDate().getYear()), record));
+		resultCollector.next(new Pair<>(record.getPlatform()+Integer.toString(record.getPublicationDate().getYear()+1), record));
+		}
+		resultCollector.next(new Pair<>(record.getPlatform(), record));
+			
+	}
+}
