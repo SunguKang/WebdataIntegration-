18e25efcc17c15e255cbbdb3d4ad3110020c378c
Florian Vincent Lang <langvincent1@web.de>
2022-11-17 10:43:25 +0100

    updated preprocessing

--- python_files/preprocessing.py
+++ python_files/preprocessing.py
@@ -8,10 +8,10 @@ import os
 from xml.etree import ElementTree
 
 #path to folder with 
-path_xml = r"..\data\schema_mapping\integrated_target_schema_xml".replace("\\","/")
-path_mapping = r"../data/schema_mapping"
-path_schema_csv = r"../data/schema_mapping/integrated_target_schema_csv"
-gold_path = r"../data/gold_standard/gold_standard_leon"
+path_xml = r".\data\schema_mapping\integrated_target_schema_xml".replace("\\","/")
+path_mapping = r"./data/schema_mapping"
+path_schema_csv = r"./data/schema_mapping/integrated_target_schema_csv"
+gold_path = r"./data/gold_standard/gold_standard_leon"
 #the paths to the original .csv datasets  
 paths = [r"integrated_target_schema_Windows.csv"
 ,r"integrated_target_schemaPS4.csv"
@@ -20,7 +20,7 @@ paths = [r"integrated_target_schema_Windows.csv"
 ,r"wikidata_integrated_target_schema.csv"]
 
 #path to godl folder
-pre_pro_path = r"../data/preprocessing".replace("\\","/")
+pre_pro_path = r"./data/preprocessing".replace("\\","/")
 
 #the paths to the matches
 c_paths= [r"A-B.csv",
@@ -101,11 +101,11 @@ def xml_to_csv(path_source_folder, path_target_folder = "", attributes = ['id','
                 
 #create .csv Files from the .xml files using the function
 xml_to_csv(path_source_folder=path_xml, path_target_folder=path_schema_csv)
-#save integrated scemas also as Dataset_X.csv (make shure the subfolder "integratet_target_schema_csv" exists)
+#save integrated schemas also as Dataset_X.csv (make shure the subfolder "integratet_target_schema_csv" exists)
 for n in names:
-    csv = pd.read_csv(paths[n])
-    csv.to_csv(path_schema_csv+"/Dataset_"+n+".csv", sep=";", index=False)
-    
+    csv_df = pd.read_csv(paths[n])
+    csv_df.to_csv(path_schema_csv+"/Dataset_"+ n +".csv", sep=";", index=False)
+
 
 #find all platform names in dataset
 
@@ -113,44 +113,68 @@ for n in names:
 #export them
 #pd.DataFrame(all_platforms).to_csv(path_to_folder+ "/" + "platforms"+".csv",index=False, sep=";")  
 #reimport them (after they were edited and matched by hand)
-platforms = pd.read_csv(pre_pro_path+"/platforms.csv", sep =";", header=None)
+platforms = pd.read_csv(pre_pro_path+"/platforms2.csv", sep =";", index_col=None)
 
 #transpose
-platforms = platforms.transpose()
-#select ony the ones in c (imputed for the ones in a)
-platforms = platforms.loc[~platforms.isnull()[3],range(0,5)]
+
+unified_platforms_column = 2
+#platforms.columns = platforms.iloc[0]
 #drop the first row
-platforms = platforms.drop(0)
+#platforms = platforms.iloc[1:]
+
+#select only the ones in c (imputed for the ones in a)
+#platforms = platforms.loc[~platforms.isnull()[truth_platforms_column],range(len(names))]
+
 #replace strings in with string lists
-for j in range (0,len(platforms.columns)):
-    for i in range(0,len(platforms)):
-        try:
-            platforms.iloc[i,j] = platforms.iloc[i,j].split(",")
-        except AttributeError:
-            if not math.isnan(platforms.iloc[i,j]):
-                print("ERROR was not detected as string, col:",j,"row:",i, platforms.iloc[i,j])    
-    
+# for j in range (0,len(platforms.columns)):
+#     for i in range(0,len(platforms)):
+#         try:
+#             platforms.iloc[i,j] = platforms.iloc[i,j].split(",")
+#         except AttributeError:
+#             if not math.isnan(platforms.iloc[i,j]):
+#                 print("ERROR was not detected as string, col:",j,"row:",i, platforms.iloc[i,j])    
+for col in platforms.columns:
+    platforms[col] = platforms[col].apply(lambda x: x.split(',') if type(x) == str else x)
+
+
 
 #change platform names, only keep the ones present in A and C (platforms[3])
 #for all datesets
-for i in range(len(names)):
-    data_csv = pd.read_csv(paths[names[i]])
+i = 0
+for dataset_name in names:
+    data_df = pd.read_csv(paths[dataset_name])
     #for all platform names
-    for j in range(0,len(platforms)):
-    #if the platform is in this dataframe
-        if type(platforms.iloc[j,i]) == list:
+    for j, row in platforms.iterrows():
+        #if the platform is in this dataframe
+        
+        if type(row[i]) == list and type(row[unified_platforms_column]) == list:
+            unified_platforms_cur = row[unified_platforms_column]
+            cur_platform = row[i][0]
+            #if cur_platform in truth_platforms_cur:
             #find matching platform name and replace with entry from dataset C
-            data_csv["platform"] = np.where(data_csv["platform"].isin(platforms.iloc[j,i]),platforms.iloc[j,3],data_csv["platform"])
+            print("platform: {}".format(row[i]))
+            selection = data_df.loc[data_df["platform"] == cur_platform, :] 
+            
+            data_df.loc[data_df["platform"] == cur_platform, 'platform'] = unified_platforms_cur[0]
+                #data_df["platform"] = np.where(data_df["platform"].isin(platforms.iloc[j,i]),platforms.iloc[j,truth_platforms_column],data_df["platform"])
+    
     #delete from dataset5
-    if("E"== names[i]):
-        p = [item for sublist in platforms[3] for item in sublist]
-        cdata_csvsv = data_csv[data_csv["platform"].isin(p)]
-    #save as _uni_plat.csv
+    if("E"== dataset_name):
+        p = [item for sublist in platforms.iloc[:,[unified_platforms_column]].dropna().values for item in sublist]
+        p = [item for sublist in p for item in sublist]
+        data_df = data_df[data_df["platform"].isin(p)]
+
     #############################################################
     # TODO insert other preprocessing here 
     
     ##############################################################
-    data_csv.to_csv(pre_pro_path+"/uniform_platform_names/Dataset_"+names[i]+".csv", index=False, sep=";")
+    #save preprocessed data again
+    data_df.to_csv(pre_pro_path+"/preprocessed_csv_files/Dataset_"+ dataset_name + ".csv", index=False, sep=";")
+    
+    i += 1
+
+
+
 
 
 
